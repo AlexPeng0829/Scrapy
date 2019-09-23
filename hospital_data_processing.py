16b586d23706b5af732d1ecb9f98345b4436d7f5
AlexPeng <pengleihit@163.com>
2019-09-23 22:56:56 +0800

    modify excel_processor to write single header to output file

--- hospital_data_processing.py
+++ hospital_data_processing.py
@@ -2,43 +2,60 @@ import os
 import xlwt
 import xlrd
 
+
+class HeaderNotExistException(Exception):
+    pass
+
+
 class Excel_processer():
     
-    def __init__(self, input_path_, start_row_, max_col_limit_ = 30, max_row_limit_ = 100):
+    def __init__(self, input_path_, header_row_num_, max_col_limit_ = 30, max_row_limit_ = 100):
 
         self.input_path = input_path_
-        self.year = self.input_path.split('\\')[-1][0:4] + '-'
+        self.year = self.input_path.split('\\')[-1][0:4] + '-' # TODO Hard-coded currently
         self.header_exist_flag = False
         self.max_col_limit = max_col_limit_
         self.max_row_limit = max_row_limit_
-        self.start_row = start_row_
+        self.header_row_num = header_row_num_
         self.row_to_append = 0
 
+    def empty_string(self, intput_str):
+        if intput_str == '':
+            return True
+        for each_char in intput_str:
+            if each_char != ' ':
+                return False
+        return True
 
+    def write_header(self, cols, input_sheet, output_sheet):
+        if self.empty_string(input_sheet.cell_value(0,0)):
+            raise HeaderNotExistException("Header of first excel file dosen't exist... Quitting！")
+        for each_row in range(self.header_row_num):
+            for each_col in range(cols):
+                output_sheet.write(each_row, each_col, input_sheet.cell_value(each_row, each_col))
+            self.row_to_append += 1
+        self.header_exist_flag = True
+    
     def read_excel(self, file_name, input_sheet, output_sheet):
 
         rows = min(input_sheet.nrows, self.max_row_limit)
         cols = min(input_sheet.ncols, self.max_col_limit)
+        
+        if self.header_exist_flag == False:
+            self.write_header(cols, input_sheet, output_sheet)
 
-        # if self.header_exist_flag == False:
-        #     start_row = self.start_row - 1
-        # else:
-        #     start_row = self.start_row
-
-        for each_row in range(self.start_row, rows): 
+        for each_row in range(self.header_row_num, rows): 
             for each_col in range(1, cols):
-                # print(input_sheet.cell_value(each_row, each_col))
+                print(input_sheet.cell_value(each_row, each_col))
                 each_cell_value = input_sheet.cell_value(each_row, each_col)
-                if(each_row > self.start_row and each_col == 1):
+                if each_col == 1:
                     next_col_value = input_sheet.cell_value(each_row, each_col + 1)
                     if each_cell_value == '' and next_col_value == '':
-                        # self.header_exist_flag == True
                         return
                 output_sheet.write(self.row_to_append, each_col, each_cell_value)
             date_prefix = self.year + file_name.split('.')[0]
             output_sheet.write(self.row_to_append, 0, date_prefix)
             self.row_to_append += 1
-        # self.header_exist_flag == True
         return
 
     def rename_file_name(self, base_folder, old_file_name):
@@ -57,14 +74,14 @@ class Excel_processer():
 
 def main():
     
-    input_path = "D:\\tmp\\静中心手术量\\2018手术安排"
+    input_path = "D:\\tmp\\静中心手术量\\2019test"
     # input_path = "D:\\tmp\\静中心手术量\\2019"
     output_file = os.path.join(input_path, "overall.xls")
-    start_row = 2
+    header_row_num = 4
 
-    excel_processer= Excel_processer(input_path, start_row)
+    excel_processer= Excel_processer(input_path, header_row_num)
     output_workbook = xlwt.Workbook()
-    output_sheet = output_workbook.add_sheet("Sheet1")
+    output_sheet = output_workbook.add_sheet("Sheet1", cell_overwrite_ok=True)
     month_folder = os.listdir(input_path)
     print(month_folder)
 
