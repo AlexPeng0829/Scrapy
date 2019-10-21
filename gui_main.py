d9f30cef201b5f82b792939391128b23091403d2
AlexPeng <pengleihit@163.com>
2019-10-21 22:30:43 +0800

    first implementation of GUI

---
+++ gui_main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from hospital_data_scrap import scrap_data
+
+class Gui_Window(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('手术数据批量爬取')
+        self.geometry('500x400')
+        self.main_frame = Frame(self, width = 500, height = 400, bg= 'lightgrey')
+        # self.main_frame.pack()
+        # label = Label(self.main_frame, text = 'hello world')
+        # label.pack(side = TOP)
+        
+        Label(self.master, text = '开始日期：').grid(row = 0, column = 0, padx = 5, pady =5)
+        Label(self.master, text = '结束日期：').grid(row = 1, column = 0, padx = 5, pady =5)
+        Label(self.master, text = '存储路径：').grid(row = 2, column = 0, padx = 5, pady =5)
+        Label(self.master, text = '当前进度：').grid(row = 3, column = 0, padx = 5, pady =5)
+        Label(self.master, text = '预计时间：').grid(row = 4, column = 0, padx = 5, pady =5)
+
+
+        v1 = StringVar(self.master, '2018-01-01')
+        v2 = StringVar(self.master, '2019-10-01')
+        v3 = StringVar(self.master, 'D:\\code\\data.xls')
+        v4 = StringVar(self.master)
+        v5 = StringVar(self.master)
+
+
+        self.start_date_entry = Entry(self.master, textvariable = v1)
+        self.end_date_entry = Entry(self.master, textvariable = v2 )
+        self.stored_dir_entry = Entry(self.master, textvariable = v3)
+        self.current_progress_entry = Entry(self.master, textvariable = v4 )
+        self.remaining_time_entry = Entry(self.master, textvariable = v5)
+
+        self.start_date_entry.grid(row = 0, column = 1)
+        self.end_date_entry.grid(row = 1, column = 1)
+        self.stored_dir_entry.grid(row = 2, column = 1)
+        self.current_progress_entry.grid(row = 3, column = 1)
+        self.remaining_time_entry.grid(row = 4, column = 1)
+
+        self.start_button = Button(self.master, text="开始", height = 2, width = 10, command=self.start_query)
+        self.start_button.grid(row = 5, column = 0, padx = 30, pady = 30)
+        self.stop_button = Button(self.master, text="停止", height = 2, width = 10, command=self.stop_query)
+        self.stop_button.grid(row = 5, column = 1, padx = 30, pady = 30)
+        # self.start_button.pack(side=BOTTOM, fill=X)
+
+    def start_query(self):
+        scrap_data(self.start_date_entry.get(), self.end_date_entry.get(), self.stored_dir_entry.get())
+
+    def stop_query(self):
+        pass
+
+    def estimate_remaing_time(self):
+        pass
+
+    def publish_current_date(self):
+        pass
+
+
+if __name__ == "__main__":
+    gui_window = Gui_Window()
+    gui_window.mainloop()
--- hospital_data_scrap.py
+++ hospital_data_scrap.py
@@ -4,22 +4,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import xlwt
+from collections import namedtuple
 
 
 
 class web_site:
 
-    def __init__(self):
+    def __init__(self, _store_dir):
         url = 'http://oa.n-huashan.org.cn:58888/OADemo/Index.aspx'
         self.url = url
         self.row_to_append = 0
         self.header_exist = False
+        self.stored_dir = _store_dir
 
         options = webdriver.ChromeOptions()
         options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
         options.add_experimental_option('excludeSwitches', ['enable-automation']) 
 
-        self.browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+        # self.browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+        self.browser = webdriver.Chrome(options=options)
         # self.wait = WebDriverWait(self.browser, 10) #超时时长为10s
 
 
@@ -33,9 +36,9 @@ class web_site:
         self.header_exist = False
         return sheet
 
-    def iterate_over_date(self):
+    def iterate_over_date(self, start_date=None, end_date=None):
         #TODO Now only iterate over September
-        dates = self.date_generator()
+        dates = self.date_generator(start_date, end_date)
         workbook = xlwt.Workbook()
 
         for dates_per_year in dates:
@@ -43,23 +46,49 @@ class web_site:
             for each_date in dates_per_year:
                 self.search(each_date)
                 self.parse_data(each_date, sheet)
-        workbook.save("D:\\code\\data.xls")
+        workbook.save(self.stored_dir)
 
-    def date_generator(self):
+    def date_generator(self, _start_date, _end_date):
         dates = list()
-        for year in range(2018, 2020):
+        Date = namedtuple('Date',['Year', 'Month', 'Day'])
+        start_date = Date(int(_start_date.split('-')[0]), int(_start_date.split('-')[1]), int(_start_date.split('-')[2]))
+        end_date = Date(int(_end_date.split('-')[0]), int(_end_date.split('-')[1]), int(_end_date.split('-')[2]))
+        
+        begin_month_flag = True
+        begin_day_flag = True
+
+        for year in range(start_date.Year, end_date.Year + 1):
             dates_per_year = list()
-            for month in range(1, 13):
+            if year == end_date.Year:
+                end_month = end_date.Month
+            else:
+                end_month = 12
+            
+            if begin_month_flag:
+                start_month = start_date.Month
+                begin_month_flag = False
+            else:
+                start_month = 1
+
+            for month in range(start_month, end_month + 1):
                 if month in (1, 3, 5, 7, 8, 10 ,12):
-                    end_day = 32
-                elif month ==2:
-                    end_day = 29
-                elif month in (4, 6, 9, 11):
                     end_day = 31
+                elif month ==2:
+                    end_day = 28
+                elif month in (4, 6, 9, 11):
+                    end_day = 30
                 else:
                     print("Value error")
                     # raise(ValueError)
-                for day in range(1, end_day):
+                if year == end_date.Year and month == end_date.Month:
+                    end_day = end_date.Day
+                if begin_day_flag:
+                    start_day = start_date.Day
+                    begin_day_flag = False
+                else:
+                    start_day = 1
+                
+                for day in range(start_day, end_day + 1):
                     date = str(year) + '-' + str(month).rjust(2, '0') + '-' + str(day).rjust(2, '0')
                     dates_per_year.append(date)
             dates.append(dates_per_year)
@@ -92,10 +121,15 @@ class web_site:
             self.row_to_append+=1
         self.header_exist = True        
 
+def scrap_data(start_date=None, end_date=None, stored_dir = None):
+    # chromedriver_path = "C:\\Users\\jiasu\\Downloads\\chromedriver_win32\\chromedriver.exe" 
+    a = web_site(stored_dir)
+    a.login() 
+    a.iterate_over_date(start_date, end_date)
+
+def main():
+    # scrap_data('2018-01-01', '2019-10-10', 'D:\\data.xls')
+    scrap_data('2018-12-23', '2019-02-10', 'D:\\data.xls')
 
 if __name__ == "__main__":
-    
-    # chromedriver_path = "C:\\Users\\jiasu\\Downloads\\chromedriver_win32\\chromedriver.exe" 
-    a = web_site()
-    a.login() 
-    a.iterate_over_date()
+    main()
